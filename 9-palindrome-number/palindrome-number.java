41cbac88497c5ac12c3fd53715093f81850d50af
Satishpenumalla <159630102+Satishpenumalla@users.noreply.github.com>
2026-08-04 12:06:23 +0530

    Time: 4 ms (100.00%) | Memory: 46 MB (53.78%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Negative or ends with 0 (but not 0 itself)
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // Even digits: x == reversedHalf
+        // Odd digits: x == reversedHalf / 10
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+}
